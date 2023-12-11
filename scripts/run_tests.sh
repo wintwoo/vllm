f3a85dc9c8b948816a9b6b04d1c92bff684fdf7d
Win Woo <wwoo@google.com>
2023-12-11 15:59:07 +1100

    Update benchmarking script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,57 +5,69 @@ VLLM_CLIENT=[VLLM_CLIENT]
 VLLM_PORT=8000
 ZONE=asia-southeast1-b
 
-OUTPUT_DIR=[OUTPUT_DIR]
-VLLM_SRC=[VLLM_SRC_PATH]
-VLLM_DATASET=[VLLM_DATASET]
-VLLM_TOKENIZER=[VLLM_TOKENIZER]
+OUTPUT_DIR=[OUTPUT_DIR] # benchmark result files
+VLLM_SRC=[VLLM_SRC] # where you cloned this repo
+VLLM_DATASET=[VLLM_DATASET] # path to the VLLM benchmark dataset
+VLLM_TOKENIZER=[VLLM_TOKENIZER] # path to the Llama2 tokenizer files
 
-LLAMA2_7B_DIR=/mnt/disks/data/llama2-7b-chat-hf
-LLAMA2_13B_DIR=/mnt/disks/data/llama2-13b-chat-hf
-LLAMA2_17B_DIR=/mnt/disks/data/llama2-70b-chat-hf
-
-# L4_7B="$LLAMA2_7B_DIR,1,1,1:2:4
-# $LLAMA2_7B_DIR,2,1,1:2:4
-# $LLAMA2_7B_DIR,4,1,1:2:4
-# $LLAMA2_7B_DIR,8,1,1:2:4"
-
-# L4_13B="$LLAMA2_13B_DIR,2,1,1:2:4
-# $LLAMA2_13B_DIR,4,1,1:2:4
-# $LLAMA2_13B_DIR,8,1,1:2:4"
-
-# L4_70B="$LLAMA2_70B_DIR,4,1,1:2:4
-# $LLAMA2_70B_DIR,8,1,1:2:4"
-
-L4_7B="$LLAMA2_7B_DIR,l4,1,inf:1:2:4"
+LLAMA2_7B_DIR=[LLAMA2_7B_DIR] # 7B pretrained weights dir
+LLAMA2_13B_DIR=[LLAMA2_13B_DIR] # 13B pretrained weights dir
+LLAMA2_70B_DIR=[LLAMA2_70B_DIR] # 70B pretrained weights dir
 
 
-while IFS= read -r line; do
-    tokens=(${line//,/ })
-    echo Model: ${tokens[0]}, GPU type: ${tokens[1]}, TP size: ${tokens[2]}
-    # start vllm server
-    gcloud compute ssh $VLLM_HOST --zone $ZONE --command \
-        "source /etc/profile && screen -L -Logfile /tmp/out.log -d -m python3 -m vllm.entrypoints.api_server --model ${tokens[0]} --disable-log-requests --tensor-parallel-size ${tokens[2]} --swap-space 16 --max-model-len 2048"
-    # wait for vllm server to start
-    while true; do
-        echo Waiting a few minutes for vllm server ...
-        sleep 120
-        last_log=$(gcloud compute ssh $VLLM_HOST --zone $ZONE --command "tail -n1 /tmp/out.log")
-        if [[ "$last_log" =~ ^.*Uvicorn[[:space:]]running.*$ ]]; then
-            break
-        else
+# Benchmarks to run - format:
+# BENCH="model_dir,gpu_type,tensor_parallel_size,request_rate_1:request_rate_2:request_rate_3 ..."
+#
+# Pass to the run_benchmark function using
+# run_benchmark $BENCH
 
-            echo Waiting longer, output is [$last_log] ...
-        fi
-    done
-    # run benchmarks at different request rates
-    IFS=':' read -ra request_rates <<< ${tokens[3]}
-    for i in "${request_rates[@]}"; do
-        echo Running benchmark at $i qps
-        gcloud compute ssh $VLLM_CLIENT --zone $ZONE --command \
-            "source /etc/profile && cd $VLLM_SRC && python3 benchmarks/benchmark_serving.py --backend vllm --host $VLLM_HOST --port $VLLM_PORT --gpu_type ${tokens[1]} --tokenizer $VLLM_TOKENIZER --dataset $VLLM_DATASET --output_dir $OUTPUT_DIR --num-prompts 10 --request-rate $i"
-    done
-    # kill vllm server
-    gcloud compute ssh $VLLM_HOST --zone $ZONE --command \
-        "pkill -U $(whoami) python3"
-    sleep 30
-done <<< "$L4_7B"
+L4_7B="$LLAMA2_7B_DIR,l4,1,inf:1:2:3:4
+$LLAMA2_7B_DIR,l4,2,inf:1:2:3:4,5,6
+$LLAMA2_7B_DIR,l4,4,inf:1:2:3:4,5,6:7:8"
+
+L4_13B="$LLAMA2_13B_DIR,l4,2,inf:1:2:3:4
+$LLAMA2_13B_DIR,l4,4,inf:1,2:3:4:5:6
+$LLAMA2_13B_DIR,l4,8,inf:1:2:3:4,5,6,7,8"
+
+L4_70B="$LLAMA2_70B_DIR,l4,2,inf:1:2:3:4
+$LLAMA2_70B_DIR,l4,4,inf:1,2:3:4:5:6
+$LLAMA2_70B_DIR,l4,8,inf:1:2:3:4,5,6,7,8"
+
+TEST_7B="$LLAMA2_7B_DIR,l4,1,inf:1:2:3:4"
+
+run_benchmark() {
+    while IFS= read -r line; do
+        tokens=(${line//,/ })
+        echo Model: ${tokens[0]}, GPU type: ${tokens[1]}, TP size: ${tokens[2]}
+        # start vllm server
+        gcloud compute ssh $VLLM_HOST --zone $ZONE --command \
+            "source /etc/profile && screen -L -Logfile /tmp/out.log -d -m python3 -m vllm.entrypoints.api_server --model ${tokens[0]} --disable-log-requests --tensor-parallel-size ${tokens[2]} --swap-space 16 --max-model-len 2048"
+        # wait for vllm server to start
+        while true; do
+            echo Waiting a few minutes for vllm server ...
+            sleep 120
+            last_log=$(gcloud compute ssh $VLLM_HOST --zone $ZONE --command "tail -n1 /tmp/out.log")
+            if [[ "$last_log" =~ ^.*Uvicorn[[:space:]]running.*$ ]]; then
+                break
+            else
+                echo Waiting longer, output is [$last_log] ...
+            fi
+        done
+        # run benchmarks at different request rates
+        IFS=':' read -ra request_rates <<< ${tokens[3]}
+        for i in "${request_rates[@]}"; do
+            echo Running benchmark at $i qps with tp_size ${tokens[2]}
+            gcloud compute ssh $VLLM_CLIENT --zone $ZONE --command \
+                "source /etc/profile && cd $VLLM_SRC && python3 benchmarks/benchmark_serving.py --backend vllm --host $VLLM_HOST --port $VLLM_PORT --gpu_type ${tokens[1]} --tokenizer $VLLM_TOKENIZER --dataset $VLLM_DATASET --output_dir $OUTPUT_DIR --num-prompts 1000 --request-rate $i --tp_size ${tokens[2]} --fixed_output_length"
+        done
+        # kill vllm server
+        echo Stopping vllm server ...
+        gcloud compute ssh $VLLM_HOST --zone $ZONE --command \
+            "pkill -9 -U $(whoami) python3"
+        sleep 15
+    done <<< "$1"
+}
+
+run_benchmark $L4_7B
+run_benchmark $L4_13B
+run_benchmark $L4_70B
